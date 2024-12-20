011e6809960256d0d54fb36001f6679e48617b62
nothingbutCode4now <shreekar.shredder2002@gmail.com>
2024-12-20 09:44:46 -0500

    added panel component

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,11 +1,11 @@
-<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+<x-panel class="flex gap-x-6">
     <div>
         <x-employer-logo />
     </div>
 
     <div class="flex-1 flex flex-col">
         <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
-        <h3 class="font-bold text-xl mt-3">Video Producer</h3>
+        <h3 class="text-xl mt-3 group-hover:text-blue-600 font-bold">Video Producer</h3>
         <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
     </div>
 
@@ -14,4 +14,4 @@
         <x-tag>Tag</x-tag>
         <x-tag>Tag</x-tag>
     </div>
-</div>
+</x-panel>
