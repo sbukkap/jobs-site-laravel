495897633edef98f382147985e517cd721f6af38
nothingbutCode4now <shreekar.shredder2002@gmail.com>
2024-12-20 03:00:30 -0500

    welcome page edit and adding import.meta.glob for builds

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,12 +6,14 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap"
-        rel="stylesheet">
+        rel="stylesheet"> -->
     <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+    @vite(['resources/js/app.js'])
+
 </head>
 
 <body class="bg-black text-white font-hanken-grotesk pb-20">
