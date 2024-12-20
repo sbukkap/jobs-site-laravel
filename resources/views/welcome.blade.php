80eb87994e60c85873de22643ad96500c18cf360
nothingbutCode4now <shreekar.shredder2002@gmail.com>
2024-12-20 10:10:34 -0500

    lets find ur next job!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,16 @@
 <x-layout>
 
     <div class="space-y-10">
+        <section class="text-center pt-6">
+            <h1 class="font-bold text-4xl">Let's Find Your Next Job</h1>
+
+            <form action="" class="mt-6">
+                <input type="text" placeholder="Web Developer..."
+                    class="rounded-xl bg-white/25 border-white/10 px-5 py-4 w-full max-w-xl">
+            </form>
+        </section>
+
+
         <section>
             <x-section-heading>Featured Jobs</x-section-heading>
 
