aed054099c73022be439abef999de95950bc1ba5
nothingbutCode4now <shreekar.shredder2002@gmail.com>
2024-12-29 18:37:51 -0500

    built create and store methods for /register route

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\JobController::class, 'index']);
+Route::get('/register', [\App\Http\Controllers\RegisteredUserController::class, 'create']);
+Route::post('/register', [\App\Http\Controllers\RegisteredUserController::class, 'store']);
+
+Route::get('/login', [\App\Http\Controllers\SessionController::class, 'create']);
+Route::post('/login', [\App\Http\Controllers\SessionController::class, 'store']);
+
+Route::delete('/logout', [\App\Http\Controllers\SessionController::class, 'destroy']);
