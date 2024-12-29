ee1127d1994de1237035e59ea4c28401e7c055e2
nothingbutCode4now <shreekar.shredder2002@gmail.com>
2024-12-29 15:04:24 -0500

    added tags model and migrations, and a job_tag pivot table

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,10 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     /** @use HasFactory<\Database\Factories\JobFactory> */
+    
     use HasFactory;
+
+    public function tag(string $name): void
+    {
+
+    }
+
+    public function tags() 
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 
     public function employer(){
         return $this->belongsTo(Employer::class);
     }
+
+
 
 }
