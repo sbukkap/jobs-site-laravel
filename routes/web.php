b98875849a87fbcc223474214a335105c77ccf87
nothingbutCode4now <shreekar.shredder2002@gmail.com>
2024-12-29 20:55:43 -0500

    added create job posting and logout functionalities

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,13 @@ use \App\Http\Controllers\RegisteredUserController;
 use \App\Http\Controllers\SessionController;
 use \App\Http\Controllers\SearchController;
 use \App\Http\Controllers\TagController;
+use \App\Http\Controllers\JobController;
 
-Route::get('/', [\App\Http\Controllers\JobController::class, 'index']);
+Route::get('/', [JobController::class, 'index']);
+
+Route::get('/jobs/create', [JobController::class, 'create'])->middleware('auth');
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
+
 Route::get('/search', SearchController::class);
 Route::get('/tags/{tag:name}', TagController::class);
 
