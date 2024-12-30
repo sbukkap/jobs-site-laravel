c9a45096c31280173d2f5faf4b85ce37aad5b4b2
nothingbutCode4now <shreekar.shredder2002@gmail.com>
2024-12-29 19:13:07 -0500

    implemented search controller and results view

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\RegisteredUserController;
 use \App\Http\Controllers\SessionController;
+use \App\Http\Controllers\SearchController;
 
 Route::get('/', [\App\Http\Controllers\JobController::class, 'index']);
+Route::get('/search', SearchController::class);
 
 Route::middleware('guest')->group(function () {
     Route::get('/register', [RegisteredUserController::class, 'create']);
